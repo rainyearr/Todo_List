e03ffbacf02f77052a1a5b18313b381a47765a8c
Rainier Dimasacat <49300740+rainyearr@users.noreply.github.com>
2021-08-31 20:56:10 +0800

    Added Task component

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Task from './components/Task';
 
 export default function App() {
   return (
@@ -12,6 +13,8 @@ export default function App() {
 
         <View style={styles.items}>
           {/* This is where the task will go!*/}
+          <Task text={'Task 1'}/>
+          <Task text={'Task 2'}/>
         </View>
       </View> 
     </View>
