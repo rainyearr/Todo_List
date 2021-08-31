ea226d702cd0a88b20db8b57f028e7dbca9bc2f8
Rainier Dimasacat <49300740+rainyearr@users.noreply.github.com>
2021-08-31 19:47:38 +0800

    First commit

--- App.js
+++ App.js
@@ -5,8 +5,12 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
+
+      {/* Today's Task */}
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Today's Task</Text>
+      </View>
     </View>
   );
 }
