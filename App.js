f5a5d756e6f8248223e906cf55b0d328b3f900f5
Rainier Dimasacat <49300740+rainyearr@users.noreply.github.com>
2021-09-01 09:59:57 +0800

    Added Text Input Box and Add Button

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { KeyboardAvoidingView, Platform, StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 import Task from './components/Task';
 
 export default function App() {
@@ -17,6 +17,20 @@ export default function App() {
           <Task text={'Task 2'}/>
         </View>
       </View> 
+      
+      {/* Write a Task!!*/}
+      <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding ": "height"}
+      style={styles.writeTaskWrapper}
+      >
+        <TextInput style = {styles.input} placeholder={"Write a Task"}></TextInput>
+        
+        <TouchableOpacity>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
+
     </View>
   );
 }
@@ -37,4 +51,33 @@ const styles = StyleSheet.create({
   items:{
     marginTop: 30,
   },
+  writeTaskWrapper:{
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  input:{
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+    width: 250,
+
+  },
+  addWrapper:{
+    width: 60,
+    height: 60,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+  },
+  addText:{},
 });
